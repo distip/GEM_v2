392111803d0983b4628e15fac523c73bf1920e6f
distip <deniz.istipliler@gmail.com>
2022-09-13 17:03:07 -0500

    predictions were improved , spatial correction plot was built

--- spatial_correction.R
+++ spatial_correction.R
@@ -28,15 +28,32 @@ spectra$PLOT.ID <- factor(spectra$PLOT.ID)
 
 spectra <- subset(spectra, select = -c(X, Unnamed..0))
 
+
+spectra.new <- spectra
+spectra.new$Block <- factor(spectra.new$Block, levels= c('2', '4', '1', '3'))
+
+ggplot(spectra.new, aes(rows, ranges, color=X730)) + 
+  geom_point(size=1.5) +
+  geom_rect( aes(xmin=0.4, xmax = 51, ymin = 7.5, ymax = 12.5), fill=NA, colour='red')+
+  #annotate('rect', xmin=0, xmax = 50, ymin = 7.5, ymax = 12.5, alpha= .1)+
+  facet_wrap(Block~Trt)+
+  scale_colour_viridis() +
+  labs(title = 'Range and Row effects on leaf spectrum', caption = ' Blocks 1 and 4 = + N , 2 and 3 = -N\nred rectangles are the hybrids ')+
+  theme_classic()
+  #theme(strip.background = element_rect(color = 'black', fill = Trt))
+
 spectra <- add_column(spectra, pos = numFactor(scale(as.numeric(spectra$rows)), scale(as.numeric(spectra$ranges))), .after = 'ranges' )
 spectra <- add_column(spectra, ID = factor(rep(1, nrow(spectra))), .after= 'pos')
 
-data <- spectra[which(spectra$Block == 3),]
+data <- spectra[which(spectra$Block == 4),]
 data <- spectra
-m1 <-glmmTMB(X1400 ~ (1|genotype), data= data)
-m2 <-glmmTMB(X1400 ~ (1|genotype) + (1|ASD) , data= data)
-m3 <-glmmTMB(X1400 ~ (1|genotype) + (1|ASD) + ar1(pos + 0 | ID), data=data)
-m4 <-glmmTMB(X1400 ~ (1|Trt) + (1|genotype) + (1|ASD) + ar1(pos + 0 | ID), data= data)
-m5 <-glmmTMB(X1400 ~ (1|genotype) + (1|ASD) + (1|Trt/Block) + ar1(pos + 0 | ID), data= data)
+m1 <-glmmTMB(X750 ~ (1|genotype), data= data)
+m2 <-glmmTMB(X750 ~ (1|genotype) + ar1(pos + 0 | ID) , data= data)
+m3 <-glmmTMB(X750 ~ (1|genotype) + (1|ASD) + ar1(pos + 0 | ID), data=data)
+m4 <-glmmTMB(X750 ~ (1|Trt) + (1|genotype) + (1|ASD) + ar1(pos + 0 | ID), data= data)
+m5 <-glmmTMB(X750 ~ (1|genotype) + (1|ASD) + (1|Trt/Block) + ar1(pos + 0 | ID), data= data)
 
 anova(m1,m2,m3)
+
+
+spectra.nas <-spectra[which(is.na(spectra$X730)),]
